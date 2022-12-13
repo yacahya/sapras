dfaa4d96e35d29d79517f901c04dcbfaeceabf78
ronisky <ronisetiawant@gmail.com>
2022-12-14 00:12:57 +0700

    fix: update profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,178 +1,198 @@
 @extends('layouts.admin')
-@section('title','Profile')
+@section('title', 'Profile')
 @section('main-content')
-<!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800">{{ __('My Profile') }}</h1>
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">{{ __('My Profile') }}</h1>
 
-@if (session('success'))
-<div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
-    {{ session('success') }}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-@endif
+    @if (session('success'))
+        <div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
 
-@if ($errors->any())
-<div class="alert alert-danger border-left-danger" role="alert">
-    <ul class="pl-4 my-2">
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
+    @if ($errors->any())
+        <div class="alert alert-danger border-left-danger" role="alert">
+            <ul class="pl-4 my-2">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-<div class="row">
+    <div class="row">
 
-    <div class="col-lg-4 order-lg-2">
+        <div class="col-lg-4 order-lg-2">
 
-        <div class="card shadow mb-4">
-            <div class="card-profile-image mt-4">
-                @if(Auth::user()->image == NULL)
-                <figure class="rounded-circle avatar avatar font-weight-bold" style="font-size: 60px; height: 180px; width: 180px;" data-initial="{{ Auth::user()->name[0] }}">
-                </figure>
-                @else
-                <img src="{{url('uploads/images/profile')}}/{{Auth::user()->image}}" class="rounded-circle avatar avatar font-weight-bold" style="font-size: 60px; height: 180px; width: 180px;" />
-                @endif
-            </div>
-            <div class="card-body">
+            <div class="card shadow mb-4">
+                <div class="card-profile-image mt-4">
+                    @if (Auth::user()->image == null)
+                        <figure class="rounded-circle avatar avatar font-weight-bold"
+                            style="font-size: 60px; height: 180px; width: 180px;"
+                            data-initial="{{ Auth::user()->name[0] }}">
+                        </figure>
+                    @else
+                        <img src="{{ url('storage/uploads/images/profile') }}/{{ Auth::user()->image }}"
+                            class="rounded-circle avatar avatar font-weight-bold"
+                            style="font-size: 60px; height: 180px; width: 180px;" />
+                    @endif
+                </div>
+                <div class="card-body">
 
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center">
-                            <h5 class="font-weight-bold">{{ Auth::user()->fullName }}</h5>
-                            <p class="text-muted text-center">
-                                @if (auth()->user()->level=='Admin')
-                                Admin
-                                @endif
-                                @if (auth()->user()->level=='Operator')
-                                Operator
-                                @endif
-                            </p>
-                            <p>NIK : {{ Auth::user()->nik }}</p>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="text-center">
+                                <h5 class="font-weight-bold">{{ Auth::user()->fullName }}</h5>
+                                <p class="text-muted text-center">
+                                    @if (auth()->user()->level == 'Admin')
+                                        Admin
+                                    @endif
+                                    @if (auth()->user()->level == 'Operator')
+                                        Operator
+                                    @endif
+                                </p>
+                                <p>NIK : {{ Auth::user()->nik }}</p>
+                            </div>
+                            <form method="POST" action="{{ route('profile.update-image') }}" enctype="multipart/form-data"
+                                class=" form-horizontal">
+                                @csrf
+                                <input type="file" name="image" class="form-control">
+                                <button type="submit" class="btn btn-primary btn-block"><b>Ganti Foto Profil</b></a>
+                            </form>
                         </div>
-                        <form method="POST" action="{{ route('profile.update-image') }}" enctype="multipart/form-data" class=" form-horizontal">
-                            @csrf
-                            <input type="file" name="image" class="form-control">
-                            <button type="submit" class="btn btn-primary btn-block"><b>Ganti Foto Profil</b></a>
-                        </form>
                     </div>
                 </div>
             </div>
+
         </div>
 
-    </div>
+        <div class="col-lg-8 order-lg-1">
 
-    <div class="col-lg-8 order-lg-1">
+            <div class="card shadow mb-4">
 
-        <div class="card shadow mb-4">
+                <div class="card-body">
 
-            <div class="card-body">
+                    <form method="POST" action="{{ route('profile.update') }}" autocomplete="off">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                <form method="POST" action="{{ route('profile.update') }}" autocomplete="off">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="_method" value="PUT">
 
-                    <input type="hidden" name="_method" value="PUT">
+                        <h6 class="heading-small text-muted mb-4">User information</h6>
 
-                    <h6 class="heading-small text-muted mb-4">User information</h6>
-
-                    <div class="pl-lg-4">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="name">Name<span class="small text-danger">*</span></label>
-                                    <input type="text" id="name" class="form-control" name="name" placeholder="Name" value="{{ old('name', Auth::user()->name) }}">
+                        <div class="pl-lg-4">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="name">Name<span
+                                                class="small text-danger">*</span></label>
+                                        <input type="text" id="name" class="form-control" name="name"
+                                            placeholder="Name" value="{{ old('name', Auth::user()->name) }}">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="last_name">Last name</label>
+                                        <input type="text" id="last_name" class="form-control" name="last_name"
+                                            placeholder="Last name"
+                                            value="{{ old('last_name', Auth::user()->last_name) }}">
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="last_name">Last name</label>
-                                    <input type="text" id="last_name" class="form-control" name="last_name" placeholder="Last name" value="{{ old('last_name', Auth::user()->last_name) }}">
+
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="nik">NIK<span
+                                                class="small text-danger">*</span></label>
+                                        <input type="text" id="nik" class="form-control" name="nik"
+                                            placeholder="" value="{{ old('nik', Auth::user()->nik) }}">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="email">Email<span
+                                                class="small text-danger">*</span></label>
+                                        <input type="email" id="email" class="form-control" name="email"
+                                            placeholder="example@example.com"
+                                            value="{{ old('email', Auth::user()->email) }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="temp_lahir">Tempat Lahir</label>
+                                        <input type="text" id="temp_lahir" class="form-control" name="temp_lahir"
+                                            placeholder="" value="{{ old('temp_lahir', Auth::user()->temp_lahir) }}">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="tgl_lahir">Tanggal Lahir</label>
+                                        <input type="date" id="tgl_lahir" class="form-control" name="tgl_lahir"
+                                            placeholder="" value="{{ old('tgl_lahir', Auth::user()->tgl_lahir) }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <label class="form-control-label" for="alamat">Alamat<span
+                                                class="small text-danger">*</span></label>
+                                        <textarea type="text" id="alamat" class="form-control" name="alamat" placeholder="Alamat">{{ old('alamat', Auth::user()->alamat) }}</textarea>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="current_password">Current password</label>
+                                        <input type="password" id="current_password" class="form-control"
+                                            name="current_password" placeholder="Current password">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="new_password">New password</label>
+                                        <input type="password" id="new_password" class="form-control"
+                                            name="new_password" placeholder="New password">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="confirm_password">Confirm password</label>
+                                        <input type="password" id="confirm_password" class="form-control"
+                                            name="password_confirmation" placeholder="Confirm password">
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="nik">NIK<span class="small text-danger">*</span></label>
-                                    <input type="text" id="nik" class="form-control" name="nik" placeholder="" value="{{ old('nik', Auth::user()->nik) }}">
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="email">Email<span class="small text-danger">*</span></label>
-                                    <input type="email" id="email" class="form-control" name="email" placeholder="example@example.com" value="{{ old('email', Auth::user()->email) }}">
+                        <!-- Button -->
+                        <BR>
+                        <div class="pl-lg-4">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <button type="submit" class="btn btn-primary">Save Changes</button>
                                 </div>
                             </div>
                         </div>
+                    </form>
 
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="temp_lahir">Tempat Lahir</label>
-                                    <input type="text" id="temp_lahir" class="form-control" name="temp_lahir" placeholder="" value="{{ old('temp_lahir', Auth::user()->temp_lahir) }}">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="tgl_lahir">Tanggal Lahir</label>
-                                    <input type="date" id="tgl_lahir" class="form-control" name="tgl_lahir" placeholder="" value="{{ old('tgl_lahir', Auth::user()->tgl_lahir) }}">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="form-group">
-                                    <label class="form-control-label" for="alamat">Alamat<span class="small text-danger">*</span></label>
-                                    <textarea type="text" id="alamat" class="form-control" name="alamat" placeholder="Alamat">{{ old('alamat', Auth::user()->alamat) }}</textarea>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="current_password">Current password</label>
-                                    <input type="password" id="current_password" class="form-control" name="current_password" placeholder="Current password">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="new_password">New password</label>
-                                    <input type="password" id="new_password" class="form-control" name="new_password" placeholder="New password">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group focused">
-                                    <label class="form-control-label" for="confirm_password">Confirm password</label>
-                                    <input type="password" id="confirm_password" class="form-control" name="password_confirmation" placeholder="Confirm password">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Button -->
-                    <BR>
-                    <div class="pl-lg-4">
-                        <div class="row">
-                            <div class="col text-center">
-                                <button type="submit" class="btn btn-primary">Save Changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
 
             </div>
 
         </div>
 
     </div>
-
-</div>
 
 @endsection
